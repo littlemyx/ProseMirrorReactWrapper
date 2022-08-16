3db381b471976e7f47caff6e01b1bbc2a9b7d7bd
Francisco Hodge <>
2022-08-16 18:55:17 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   prose-mirror-react-wrapper v1.0.9
+ *   prose-mirror-react-wrapper v1.0.10
  *   https://github.com/littlemyx/ProseMirrorReactWrapper
  *
  *   Copyright (c) Andrei Mukhin (https://631am.com) and project contributors.
