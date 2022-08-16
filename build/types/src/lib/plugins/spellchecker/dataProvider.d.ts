205156226e9f30730e9795301c81c3eacae6c310
Francisco Hodge <>
2022-08-16 19:27:38 +0000

    Build update

--- build/types/src/lib/plugins/spellchecker/dataProvider.d.ts
+++ build/types/src/lib/plugins/spellchecker/dataProvider.d.ts
@@ -1,9 +1,7 @@
 import type { DataProvider } from "../dataProvider";
 import { Word, Error } from "./types";
-declare class LocalDataProvider implements DataProvider<Word[], Error[]> {
+export default class LocalDataProvider implements DataProvider<Word[], Error[]> {
     private abortionController;
     get getAbortionControllerHandler(): () => void;
     requestData(words: Word[]): Promise<Error[]>;
 }
-export default LocalDataProvider;
-export { DataProvider };
